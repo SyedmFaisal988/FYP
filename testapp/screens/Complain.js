1b06de63e04e992a596159b8d3f204f863841dbe
--global <syed.mfaisal988@gmail.com>
2020-10-10 20:20:51 +0500

    voice recording.

--- testapp/screens/Complain.js
+++ testapp/screens/Complain.js
@@ -7,11 +7,12 @@ import {
   Text,
   KeyboardAvoidingView,
   ScrollView,
+  Vibration,
 } from "react-native";
-import { Upload, AddFile } from "../components/icons";
+import { Upload, AddFile, ImageIcon, VideoIcon, AudioIcon } from "../components/icons";
 import InputField from "../components/InputFields";
 import * as ImagePicker from "expo-image-picker";
-import { Audio } from 'expo-av';
+import { Audio } from "expo-av";
 import Header from "../components/Header";
 
 const styles = StyleSheet.create({
@@ -24,7 +25,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#eaeaea",
-    elevation: 10
+    elevation: 10,
   },
   labelText: {
     fontSize: 17,
@@ -36,24 +37,35 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   addFileButton: {
+    marginLeft: 0
+  },
+  attachmentButtons: {
+    backgroundColor: "#eaeaea",
     width: 100,
     height: 100,
-    backgroundColor: "#eaeaea",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
     elevation: 10,
-  },
+    marginHorizontal: 15,
+    marginBottom: 10
+  }
 });
+
+let recording = null;
 
 class Complain extends React.Component {
   state = {
     imageUrl: "",
     subject: "",
     description: "",
-    formData: {
-      image: null,
-    },
+    attachments: [{
+      type: ImagePicker.MediaTypeOptions.Images
+    }, {
+      length: 234,
+    }, {
+      uri: ''
+    }],
   };
 
   initializeComponent = async () => {
@@ -85,21 +97,68 @@ class Complain extends React.Component {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 0.4,
+      allowsMultipleSelection: true,
       exif: true,
+      
     });
+    console.log({ result });
     if (result && !result.cancelled) {
       if (isCover) {
         this.setState({
           imageUrl: result.uri,
         });
       } else {
-        return console.log(result);
+        this.setState((prevState) => ({
+          attachments: prevState.attachments.concat([
+            { type: result.type, uri: result.uri },
+          ]),
+        }));
       }
     }
   };
 
+  onRecordingAudioStart = async () => {
+    Vibration.vibrate(300);
+    recording = new Audio.Recording();
+    try {
+      await recording.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      await recording.startAsync();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  playBackRecording = async (recordingInstance) => {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync({ uri: recordingInstance._uri });
+      await soundObject.playAsync();
+      setTimeout(() => {
+        soundObject.unloadAsync().then(() => {});
+      }, recordingInstance._finalDurationMillis);
+    } catch (error) {
+      // An error occurred!
+    }
+  };
+
+  onRecordingAudioEnd = async () => {
+    if (!recording)
+      return;
+    Vibration.vibrate(100);
+    await recording.stopAndUnloadAsync();
+    await this.playBackRecording(recording);
+    this.setState((prevState) => ({
+      attachments: prevState.attachments.concat([
+        { uri: recording._uri, length: recording._finalDurationMillis },
+      ]),
+    }));
+    recording = null;
+  };
+
   render() {
-    const { imageUrl, subject, description } = this.state;
+    const { imageUrl, subject, description, attachments } = this.state;
     return (
       <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }} enabled>
         <Header text="Complain" {...this.props} />
@@ -140,12 +199,30 @@ class Complain extends React.Component {
             <View style={{ marginBottom: 15 }} />
             <Text style={styles.labelText}>Attachments</Text>
             <View style={styles.attachmentContainer}>
-              <View style={styles.addFileButton}>
-                <TouchableOpacity onPressIn={() => { console.log('press') }} onLongPress={() => {console.log('long')}} onPressOut={() => {console.log('unPress')}}>
-                  <AddFile  style={{ marginLeft: 10 }} />
-                </TouchableOpacity>
-                <Text>Add File</Text>
-              </View>
+              <TouchableOpacity
+                onPress={() => this.handlePickImage()}
+                onLongPress={this.onRecordingAudioStart}
+                onPressOut={this.onRecordingAudioEnd}
+              >
+                <View style={[styles.addFileButton, styles.attachmentButtons]}>
+                  <AddFile style={{ marginLeft: 10 }} />
+                  <Text>Add File</Text>
+                </View>
+              </TouchableOpacity>
+              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
+              {attachments.map((attachment) => (
+                <View style={styles.attachmentButtons}>
+                  {attachment.length ? (
+                    <AudioIcon />
+                  ) : attachment.type ===
+                        "image" ? (
+                    <ImageIcon />
+                  ) : (
+                    <VideoIcon />
+                  )}
+                </View>
+              ))}
+              </ScrollView>
             </View>
           </View>
           <View style={{ marginBottom: 15 }} />
