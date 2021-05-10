0c89c00e459f18b549a1b4fc158fb6a75dc95cb4
syedmfaisal988 <syed.faisal@abtach.org>
2021-05-10 17:16:20 +0500

    db path

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 exports.hostName = process.env.PORT? '0.0.0.0' : '192.168.4.104'
 exports.port = process.env.PORT || 4000
-exports.db_url =  "mongodb://localhost:27017"  
+exports.db_url =  "mongodb+srv://faisal:123_456789@cluster0.ck9eq.mongodb.net"  
 exports.db_name = "location"
 exports.secretKey = "12345-67890-09876-54321"
